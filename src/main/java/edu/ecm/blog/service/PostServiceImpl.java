7e61d8ad0a13daac0eb75220a83997412c10491d
damien bourdette <dbourdette@gmail.com>
2011-06-07 23:32:12 +0200

    done spring tp

---
+++ src/main/java/edu/ecm/blog/service/PostServiceImpl.java
@@ -0,0 +1,55 @@
+package edu.ecm.blog.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.ecm.blog.domain.Post;
+
+public class PostServiceImpl implements PostService {
+	@Inject
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public void save(Post post) {
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.save(post);
+	}
+	
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.createQuery("delete Post where id = :id").setLong("id", id).executeUpdate();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	@SuppressWarnings("unchecked")
+	public List<Post> find(int pageIndex, int pageSize) {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Criteria criteria = session.createCriteria(Post.class);
+		
+		criteria.setFirstResult(pageIndex * pageSize);
+		criteria.setMaxResults(pageSize);
+		
+		return criteria.list();
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public int count() {
+		Session session = sessionFactory.openSession();
+
+		return ((Long) session.createQuery("select count(*) from Post").uniqueResult()).intValue();
+	}
+}
--- src/test/java/edu/ecm/blog/service/PostServiceTest.java
+++ src/test/java/edu/ecm/blog/service/PostServiceTest.java
@@ -1,15 +1,20 @@
 package edu.ecm.blog.service;
 
 import java.util.Date;
+
 import javax.inject.Inject;
+
 import junit.framework.Assert;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import edu.ecm.blog.domain.Post;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,23 +23,25 @@ public class PostServiceTest {
 
     @Inject
     private SessionFactory sessionFactory;
+    
+    @Inject
+    private PostService postService;
 
     @After
     public void cleanDb() {
-        Session session = sessionFactory.openSession();
-
+    	Session session = sessionFactory.openSession();
+    	
+    	Transaction transaction = session.beginTransaction();
+    	
         session.createQuery("delete from Post").executeUpdate();
-
+        
+        transaction.commit();
+        
         session.close();
-
-        sessionFactory.close();
     }
 
     @Test
     public void save() {
-        PostService postService = new PostService();
-        postService.setSessionFactory(sessionFactory);
-
         Post post = new Post();
         post.setTitle("un post");
         post.setDate(new Date());
@@ -44,9 +51,6 @@ public class PostServiceTest {
 
     @Test
     public void delete() {
-        PostService postService = new PostService();
-        postService.setSessionFactory(sessionFactory);
-
         Post post = new Post();
         post.setTitle("un post");
         post.setDate(new Date());
@@ -64,9 +68,6 @@ public class PostServiceTest {
 
     @Test
     public void find() {
-        PostService postService = new PostService();
-        postService.setSessionFactory(sessionFactory);
-
         Post post = new Post();
         post.setTitle("un post");
         post.setDate(new Date());
@@ -85,9 +86,6 @@ public class PostServiceTest {
 
     @Test
     public void count() {
-        PostService postService = new PostService();
-        postService.setSessionFactory(sessionFactory);
-
         Post post = new Post();
         post.setTitle("un post");
         post.setDate(new Date());

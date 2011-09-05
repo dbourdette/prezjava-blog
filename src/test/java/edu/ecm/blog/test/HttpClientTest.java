5344c77ce758d6114ae5f92fedef14880458166d
damien bourdette <dbourdette@gmail.com>
2011-09-05 18:24:17 +0200

    added @Ignore to httpclient test

--- src/test/java/edu/ecm/blog/test/HttpClientTest.java
+++ src/test/java/edu/ecm/blog/test/HttpClientTest.java
@@ -9,10 +9,12 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class HttpClientTest {
 	@Test
+	@Ignore
 	public void get() throws ClientProtocolException, IOException {
 		HttpClient httpClient = new DefaultHttpClient();
 		
